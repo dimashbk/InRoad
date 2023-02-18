949ba88168e4967fb41628cc3b41d49b08af5d47
dimash <200103433@stu.sdu.edu.kz>
2023-02-18 21:10:40 +0600

    Added UISegmentControll

--- InRoad/View/HomeViewController.swift
+++ InRoad/View/HomeViewController.swift
@@ -7,23 +7,45 @@
 
 import UIKit
 import GoogleMaps
-class HomeViewController: UIViewController {
+import SnapKit
 
+class HomeViewController: UIViewController {
+    let items = ["Точки",
+                 "Cветовые схемы"]
+    var segmentControl: UISegmentedControl{
+        let control = UISegmentedControl(items: items)
+        return control
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        googleMaps()
+        setUpGoogleMaps()
+        initialize()
     }
-    func googleMaps(){
+
+    private func setUpGoogleMaps(){
         GMSServices.provideAPIKey("AIzaSyCDM2tOxkbG3s2FifKxEZPwSIMSUZveaT8")
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86,                                                  longitude:151.20,
-                                              zoom: 15.0)
-        let mapView = GMSMapView.map(withFrame: view.frame, camera: camera)
-        view.addSubview (mapView)
-        let marker = GMSMarker ()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
+        
+        let camera = GMSCameraPosition.camera(withLatitude: 43.22126814174525,                                                                             longitude:    76.85327279571138,
+                                              zoom:         15.0)
+        let mapView = GMSMapView.map(withFrame:             view.frame,
+                                     camera:                camera)
+        view.addSubview(mapView)
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude:  43.22126814174525,
+                                                 longitude: 76.85327279571138)
+        marker.title = "Almaty"
+        marker.snippet = "Kazakhstan"
         marker.map = mapView
+    }
+    private func initialize(){
+        view.addSubview(segmentControl)
+        segmentControl.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(16)
+            make.width.equalTo(100)
+            make.height.equalTo(50)
+        }
+        
     }
     
 
